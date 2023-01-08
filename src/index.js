88be468b3c3ea92b7cb315a85a651e6e46e6bd0e
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2023-01-08 21:02:10 +0200

    Hide all keys

--- src/index.js
+++ src/index.js
@@ -10,11 +10,7 @@ import { Auth0Provider } from "@auth0/auth0-react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Auth0Provider
-      domain="dev-aaj2yr6lcyzcugtn.us.auth0.com"
-      clientId="xnKFazF6C9CooLjaA9KQJVGqctLH6evb"
-      redirectUri={window.location.origin}
-    >
+    <Auth0Provider domain="" clientId="" redirectUri={window.location.origin}>
       <Provider store={store}>
         <App />
       </Provider>
